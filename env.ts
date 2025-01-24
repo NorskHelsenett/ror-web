dadf0d60cceffb19ecb9f2c981517160bf5d151c
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-01-24 13:22:16 +0100

    ✨ refactor: change env variable names to follow standard practice by next-auth and next.js

--- env.ts
+++ env.ts
@@ -1,6 +1,11 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+/**
+ * We use this library to validate needed env variables to run the project
+ *
+ */
+
 export const env = createEnv({
   /*
    * Serverside Environment variables, not available on the client.
@@ -11,14 +16,21 @@ export const env = createEnv({
     AUTH_ISSUER: z.string().url(),
     AUTH_CLIENT_ID: z.string().min(1),
     AUTH_CLIENT_SECRET: z.string().min(1),
-    TRUST_HOST: z.string().min(1),
+    AUTH_TRUST_HOST: z
+      .string()
+      // only allow "true" or "false"
+      .refine((s) => s === "true" || s === "false")
+      // transform to boolean
+      .transform((s) => s === "true")
+      .optional(),
   },
   /**
    * Environment variables available on the client (and server).
+   * Next.js uses the prefix `NEXT_PUBLIC_` to expose environment variables to the client.
    */
-  clientPrefix: "PUBLIC_",
+  clientPrefix: "NEXT_PUBLIC_",
   client: {
-    PUBLIC_ROR_API_URL: z.string().url(),
+    NEXT_PUBLIC_ROR_API_URL: z.string().url(),
   },
   /**
    * The runtime environment variables mapped to our `server` and `client` schemas.
@@ -27,11 +39,24 @@ export const env = createEnv({
    * `import.meta.env` is a Vite specific feature.
    */
   runtimeEnv: {
+    /**
+     * Authentication provider configuration.
+     * {@link /app/auth.ts}
+     */
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_ISSUER: process.env.AUTH_ISSUER,
     AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
     AUTH_CLIENT_SECRET: process.env.AUTH_CLIENT_SECRET,
-    PUBLIC_ROR_API_URL: process.env.ROR_API_URL,
-    TRUST_HOST: process.env.TRUST_HOST,
+    /**
+     * When deploying your application behind a reverse proxy, you’ll need to set AUTH_TRUST_HOST equal to true.
+     * This tells Auth.js to trust the X-Forwarded-Host header from the reverse proxy.
+     * @docs {@link https://authjs.dev/getting-started/deployment#auth_trust_host}
+     */
+    AUTH_URL_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+
+    /**
+     * The API endpoint for the ROR API.
+     */
+    NEXT_PUBLIC_ROR_API_URL: process.env.NEXT_PUBLIC_ROR_API_URL,
   },
 });
--- services/ror-api.ts
+++ services/ror-api.ts
@@ -2,9 +2,13 @@ import { env } from "@/env";
 import { createApiClient, loggingMiddleware } from "@/packages/ror-api-client";
 
 export const rorApiClient = (accessToken: string) => {
+  if (!accessToken) {
+    throw new Error("No access token provided for rorApiClient");
+  }
+
   const middlewares = [loggingMiddleware];
   const config = {
-    baseUrl: env.PUBLIC_ROR_API_URL,
+    baseUrl: env.NEXT_PUBLIC_ROR_API_URL,
     accessToken,
   };
   return createApiClient(config, middlewares);
