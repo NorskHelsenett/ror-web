b9599fdecc20ebc060dd0914177e1733baffe5e7
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-03-11 16:56:28 +0100

    feat(react): add stack component

---
+++ packages/react/src/components/stack.tsx
@@ -0,0 +1,78 @@
+import { clsx } from 'clsx'
+import type { ComponentPropsWithoutRef, CSSProperties, ReactElement, ReactNode } from 'react'
+
+/**
+ * The steps in the spacing scale
+ * {@link packages/styles/scss/spacing.scss}
+ */
+type SPACING_STEPS = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14
+
+export interface StackProps extends ComponentPropsWithoutRef<'div'> {
+  /**
+   * Provide a custom element type to render as the outermost element in
+   * the Stack component. By default, this component will render a `div`.
+   */
+  as?: ReactElement['type']
+
+  /**
+   * Provide the elements that will be rendered as children inside of the Stack
+   * component. These elements will have having spacing between them according
+   * to the `step` and `orientation` prop
+   */
+  children?: ReactNode
+
+  /**
+   * Provide a custom class name to be used by the outermost element rendered by
+   * Stack
+   */
+  className?: string
+
+  /**
+   * Provide a custom value or step from the spacing scale to be used
+   * as the gap in the layout
+   */
+  gap?: string | SPACING_STEPS
+
+  /**
+   * Specify the orientation of them items in the Stack
+   */
+  orientation?: 'horizontal' | 'vertical'
+
+  /**
+   * Specify custom style properties
+   */
+  style?: CSSProperties & { '--r-stack-gap': string }
+}
+
+export function Stack(props: StackProps) {
+  const {
+    as: Comp = 'div',
+    children,
+    className: customClassName,
+    gap = 0,
+    orientation = 'vertical',
+    style: customStyle,
+    ...rest
+  } = props
+
+  const numericGap = typeof gap === 'number' ? gap.toFixed() : ''
+
+  const className = clsx('r-stack', customClassName, {
+    [`r-stack--${orientation}`]: true,
+    [`r-stack--scale-${numericGap}`]: typeof gap === 'number' && gap > 0,
+  })
+
+  const style = {
+    ...customStyle,
+  } satisfies CSSProperties
+
+  if (typeof gap === 'string') {
+    style[`--r-stack-gap`] = gap
+  }
+
+  return (
+    <Comp {...rest} className={className} style={style}>
+      {children}
+    </Comp>
+  )
+}
