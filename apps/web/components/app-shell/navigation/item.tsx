a15e160f4ed9c3da7912c064034db30944844a80
Patrick Edqvist <patrick.edqvist@gmail.com>
2025-03-11 13:57:19 +0100

    refactor(web): add spacing and utility classes to tailwind config

--- apps/web/components/app-shell/navigation/item.tsx
+++ apps/web/components/app-shell/navigation/item.tsx
@@ -105,7 +105,7 @@ export function NavigationItem({ label, href, icon, items }: NavigationItemProps
               className={fo.flyout}
               side='right'
               align='center'
-              sideOffset={16}
+              sideOffset={8}
               onMouseEnter={handleOnContentMouseEnter}
               onMouseLeave={handleOnContentMouseLeave}
             >
