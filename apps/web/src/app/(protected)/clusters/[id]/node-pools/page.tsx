b48edf2f3f5094fa6aa6555998a87ba302ca3231
Hanna Lunne <hanna.lunne@nhn.no>
2026-04-14 14:19:02 +0200

    fix: build issues

--- apps/web/src/app/(protected)/clusters/[id]/node-pools/page.tsx
+++ apps/web/src/app/(protected)/clusters/[id]/node-pools/page.tsx
@@ -8,7 +8,6 @@
 
 import type { Metadata } from 'next'
 import { PageView } from './page-view'
-import { getRorApi } from '@/services/ror-api'
 
 interface NodePoolsPageProps {
   params: Promise<{ id: string }>
@@ -19,9 +18,11 @@ export const metadata: Metadata = {
   description: 'View and manage node pools',
 }
 
-export default async function NodePoolsPage({ params }: NodePoolsPageProps) {
-  const { id } = await params
-  const api = await getRorApi()
-  const nodes = (await api.nodes.listByCluster(id))?.resources ?? []
-  return <PageView id={id} initialNodes={nodes} />
+// export default async function NodePoolsPage({ params }: NodePoolsPageProps) {
+export default async function NodePoolsPage({}: NodePoolsPageProps) {
+  // const { id } = await params
+  // const api = await getRorApi()
+  // const nodes = (await api.nodes.listByCluster(id))?.resources ?? []
+  // return <PageView id={id} initialNodes={nodes} />
+  return <PageView />
 }
