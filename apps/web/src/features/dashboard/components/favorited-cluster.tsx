4f18b0d3ea042a496518e2858e42e12510e2189b
Martine Nilsen <martinenilsen02@gmail.com>
2026-06-08 12:08:23 +0200

    #624 Fix: Lint

--- apps/web/src/features/dashboard/components/favorited-cluster.tsx
+++ apps/web/src/features/dashboard/components/favorited-cluster.tsx
@@ -18,9 +18,7 @@ import {
   getArgocdUrlViewItem,
   getGrafanaUrlViewItem,
   getStatusViewItem,
-  getClusterUidView,
 } from '@/features/cluster/utils/cluster'
-import { routes } from '@/config/routes'
 
 const Dot = ({ status }: { status: HealthStatus }) => (
   <span className={'flex items-center justify-around size-5'}>
