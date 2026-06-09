ef2bfeb4dcbdbe3e844ad63fed97f4e2ddc7e7f9
Martine Nilsen <martinenilsen02@gmail.com>
2026-06-09 12:37:33 +0200

    #626 Fix: Try to fix the list/grid-view status

--- apps/web/src/app/(protected)/vms/page-view.tsx
+++ apps/web/src/app/(protected)/vms/page-view.tsx
@@ -391,7 +391,9 @@ export const PageView = ({ className, vms, params }: PageViewProps) => {
         expect finished functionality or that all data is present. The development team is working hard on delivering a
         complete product as quick as possible :)
       </NotReadyMessage>
-      <section className='px-12 my-8'>{params.view === 'list' ? <TableView /> : <GridView />}</section>
+      <section className='px-12 my-8'>
+        {(searchParams.get('view') ?? params.view) === 'list' ? <TableView /> : <GridView />}
+      </section>
     </div>
   )
 }
